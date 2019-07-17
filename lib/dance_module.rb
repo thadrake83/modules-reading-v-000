c6173e964ce07fdb6616d1a10feeb84e1736150d
Drake Rentrop <drakerentrop@gmail.com>
2019-07-17 13:49:15 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,5 +1,19 @@
 module Dancer
 
+def twirling
+  "I'm twirling!"
+end
 
+def jump
+  "Look how high I'm jumping!"
+end
+
+def pirouette
+  "I'm doing a prouette"
+end
+
+end take_a_bow
+"Thank you, thank you. It was a pleasure to dance for you all."
+end
 
 end
