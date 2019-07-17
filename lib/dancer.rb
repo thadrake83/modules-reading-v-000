e1351f1a7166279d91e2264eb296d72997fefaad
Drake Rentrop <drakerentrop@gmail.com>
2019-07-17 14:16:07 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,6 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-
   include Dance
 
   attr_accessor :name
@@ -10,5 +9,4 @@ class Dancer
   def initialize(name)
     @name = name
   end
-
 end
